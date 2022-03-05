7894034deff90b30d026abde69bf0e98dad1e9c9
VladFathulin <89787005+vladnum@users.noreply.github.com>
2022-03-05 13:53:44 +0500

    visual

--- main.py
+++ main.py
@@ -14,6 +14,24 @@ dp = Dispatcher(bot)
 
 db = SQLite('database.db')
 
-@dp.message_handler('start')
-async def asd():
-    pass
+sub_keyboard = types.InlineKeyboardMarkup()
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    if db.get_user_in_base(message.from_user.id) is None:
+        db.add_user_in_base(message.from_user.id, message.from_user.name)
+        await message.answer('Вы зарегестрированы', reply_markup=await keyboard('start'))
+    else:
+        await message.answer('Главное меню', reply_markup=await keyboard('start'))
+
+@dp.message_handler(commands=['queue'])
+async def start_search(message: types.Message):
+    if db.get_user_in_queue(message.from_user.id) is None:
+        check_user_in_room = db.get_user_in_room(message.from_user.id)
+        if check_user_in_room[0] is None and check_user_in_room[1] is None:
+            db.add_user_in_queue(message.from_user.id)
+            await message.answer('B поиске', reply_markup=await keyboard('cancel'))
+        else:
+            await message.answer('Вы уже общаетесь')
+    else:
+        await message.answer('Вы уже в поиске')
