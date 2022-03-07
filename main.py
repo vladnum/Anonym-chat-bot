c48007a3e5e4e22cc9a529b2b725f0a59473fa9b
Vlad;git config --global user.name Vlad <vladfathulin@gmail.com>
2022-03-07 13:29:59 +0500

    first step of project

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from ctypes import resize
 import logging
 import random
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ChatType
+from aiogram.types import ChatType, InlineKeyboardButton, InlineKeyboardMarkup
 from sqlite import SQLite
 from buttons import keyboard
 from vars import *
@@ -12,8 +13,119 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
-db = SQLite('database.db')
+db = SQLite()
 
-@dp.message_handler('start')
-async def asd():
-    pass
+list_skipped_text = ['Личный кабинет', 'Найти собеседника', 'Отменить', 'Написать в поддержку', 'Завершить чат']
+
+sup_keyboard = InlineKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+button = InlineKeyboardButton('Написать в поддержку', url='http://t.me/vdlikk')
+sup_keyboard.add(button)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    if db.get_user_in_base(message.from_user.id) is None:
+        db.add_user_in_base(message.from_user.id, message.from_user.username)
+        await message.answer('Вы зарегестрированы', reply_markup=await keyboard('start'))
+    else:
+        await message.answer('Главное меню', reply_markup=await keyboard('start'))
+
+@dp.message_handler(commands=['queue'])
+async def start_search(message: types.Message):
+    if db.get_user_in_queue(message.from_user.id) is None:
+        check_user_in_room = db.get_user_in_room(message.from_user.id)
+        if check_user_in_room[0] is None and check_user_in_room[1] is None:
+            db.add_user_in_queue(message.from_user.id)
+            await message.answer('B поиске', reply_markup=await keyboard('cancel'))
+        else:
+            await message.answer('Вы уже общаетесь')
+    else:
+        await message.answer('Вы уже в поиске')
+
+@dp.message_handler(commands=['exit'])
+async def exit(message: types.Message):
+    if db.get_user_in_queue(message.from_user.id) is None:
+        await message.answer('Вы не в поиске')
+    else:
+        user_in_room_check = db.get_user_in_room(message.from_user.id)
+        if user_in_room_check[0] is None and user_in_room_check[1] is None:
+            db.delete_user_from_queue(message.from_user.id)
+            await message.answer('Вы были удалены из поиска', reply_markup=await keyboard('start'))
+        else:
+            await message.answer('Вы уже общаетесь')
+
+@dp.message_handler(commands=['stop_chat'])
+async def stop_chating(message: types.Message):
+    room_id = db.get_user_id_room(message.from_user.id)
+    if room_id is not None:
+        room_mate_id = db.get_room_mate_id(room_id[0], message.from_user.id)
+        db.delete_room()
+        await bot.send_message(room_mate_id, 'Собеседник завершил беседу', reply_markup=await keyboard('start'))
+        await message.answer('Вы завершили беседу', reply_markup=await keyboard('start'))
+    else:
+        await message.answer('Вы ни с кем не общаетесь')
+
+@dp.message_handler(chat_type = ChatType.PRIVATE, content_types=['text', 'video', 'sticker', 'audio', 'game', 'voice', 'photo', 'animation', 'dice', 'video_note'])
+async def get_text(message: types.Message):
+    if message.text == 'Личный кабинет':
+        pass
+    elif message.text == 'Найти собеседника':
+        await start_search(message)
+    elif message.text == 'Отменить':
+        await exit(message)
+    elif message.text == 'Завершить чат':
+        await stop_chating(message)
+    elif message.text == 'Написать в поддержку':
+        await message.answer('Ведутся технические работы, проект на этапе бета-теста')
+
+    room_id = db.get_user_id_room(message.from_user.id)
+    if room_id is not None:
+        if message.text in list_skipped_text:
+            pass
+        else:
+            room_mate_id = db.get_room_mate_id(room_id[0], message.from_user.id)
+            if message.text:
+                await bot.send_message(room_mate_id, f'Собеседник: {message.text}')
+            elif message.video:
+                await bot.send_video(room_mate_id, message.video.file_id, caption='Собеседник')
+            elif message.sticker:
+                await bot.send_sticker(room_mate_id, message.sticker.file_id, caption='Собеседник')
+            elif message.audio:
+                await bot.send_audio(room_mate_id, message.audio.file_id, caption='Собеседник')
+            elif message.game:
+                await bot.send_game(room_mate_id, message.gme.file_id, caption='Собеседник')
+            elif message.voice:
+                await bot.send_voice(room_mate_id, message.voice.file_id, caption='Собеседник')
+            elif message.photo:
+                await bot.send_photo(room_mate_id, message.photo.file_id, caption='Собеседник')
+            elif message.video_note:
+                await bot.send_video_note(room_mate_id, message.video_note.file_id, caption='Собеседник')
+            elif message.dice:
+                await bot.send_dice(room_mate_id, message.dice.file_id, caption='Собеседник')
+            elif message.animation:
+                await bot.send_animation(room_mate_id, message.animation.file_id, caption='Собеседник')
+
+async def connect_users():
+    queue = db.get_queue()
+    if queue is None:
+        pass
+    elif len(queue) >= 2:
+        users_id = [queue[0][0], queue[1][0]]
+        db.delete_user_from_queue(users_id[0])
+        db.delete_user_from_queue(users_id[1])
+        await bot.send_message(users_id[0], 'Себеседник найден!', reply_markup= await keyboard('exit'))
+        await bot.send_message(users_id[1], 'Собеседник найден!', reply_markup=await keyboard('exit'))
+        rooms = len(db.get_count_rooms())
+        room_id = 0 if rooms is None else rooms+1
+        db.add_new_room(room_id, users_id[0], users_id[1])
+    else:
+        pass
+
+def repeat_connect_users(coro, lp):
+    asyncio.ensure_future(coro(), loop=lp)
+    loop.call_later(1, repeat_connect_users, coro, lp)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.call_later(5, repeat_connect_users, connect_users, loop)
+    executor.start_polling(dp, skip_updates=True)
--- sqlite.py
+++ sqlite.py
@@ -6,20 +6,20 @@ class SQLite:
     def __init__(self):
         self.database = sqlite3.connect('database.db')
         self.cursor = self.database.cursor()
-        self.cursor.execute("""TABLE IF NOT EXISTS users (
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS users (
             user_id INT,
             username TEXT,
             total_spend_time TEXT,
             time_register TEXT
         )""")
-        self.cursor.execute("""TABLE IF NOT EXISTS rooms (
-            rooms_id INT,
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS rooms (
+            room_id INT,
             first_user_id INT,
             second_user_id INT,
             started TEXT
         )""")
-        self.cursor.execute("""TABLE IF NOT EXISTS queue (
-            user_id INT
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS queue (
+            user_id INT,
             started TEXT
         )""")
         self.database.commit()
@@ -33,45 +33,45 @@ class SQLite:
         return [first_user, second_user]
 
     def get_user_in_queue(self, user_id):
-        return self.cursor.execute(user_id).fetchone()
+        return self.cursor.execute(f"SELECT user_id FROM queue WHERE user_id = {user_id}").fetchone()
 
     def get_room_mate_id(self, room_id, user_id):
         users_id = self.cursor.execute(f"SELECT first_user_id, second_user_id FROM rooms WHERE room_id = '{room_id}'").fetchall()
         return users_id[0][0] if user_id == users_id[0][1] else users_id[0][1]
 
-    def get_queue(self, room_id):
-        return self.cursor.execute(f"SELECT first_user_id, second_user_id FROM rooms WHERE room_id = '{room_id}'").fetchall()
+    def get_queue(self):
+        return self.cursor.execute("SELECT user_id FROM queue LIMIT 2").fetchall()
 
     def get_user_id_room(self, user_id):
-        return self.cursor.execute(f"SELECT room_id FROM rooms WHERE first_user_id = '{user_id}' OR second_user_id = '{user_id}'").fetchall()
+        return self.cursor.execute(f"SELECT room_id FROM rooms WHERE first_user_id = '{user_id}' OR second_user_id = '{user_id}'").fetchone()
 
     def get_count_rooms(self):
-        return self.cursor.execute(f"SELECT room_id FROM rooms").fetchall()
+        return self.cursor.execute("SELECT room_id FROM rooms").fetchall()
 
     def add_user_in_base(self, user_id, username):
-        return self.cursor.execute(
-            'INSERT INTO users',
-            "('user_id', 'username', 'total_spend_time', 'timeregister')"
-            "VALUES int(?, ?, ?, ?)"
-            (user_id, username, 0,1,0,0, datetime.now())
+        self.cursor.execute(
+            "INSERT INTO users"
+            "('user_id', 'username', 'total_spend_time', 'time_register')"
+            "VALUES (?,?,?,?)",
+            (user_id, username, 0, datetime.now())
         )
         self.database.commit()
 
     def add_user_in_queue(self, user_id):
-        self.cursor.execute(f"INSERT INTO rooms (user_id, username) VALUES (?, ?)", (user_id, datetime.now()))
+        self.cursor.execute("INSERT INTO queue ('user_id', 'started') VALUES (?,?)", (user_id, datetime.now()))
         self.database.commit()
 
     def add_new_room(self, room_id, first_user_id, second_user_id):
-        self.cursor.executor(f"INSERT INTO rooms (`room_id`, `first_user_id`, `second_user_id`, `started`) VALUES (?,?,?,?)",
+        self.cursor.execute("INSERT INTO rooms ('room_id', 'first_user_id', 'second_user_id', 'started') VALUES (?,?,?,?)",
                             (room_id, first_user_id, second_user_id, datetime.now()))
         self.database.commit()
 
     def delete_user_from_queue(self, user_id):
-        self.cursor.executor(f"DELETE FROM queue WHERE user_id = '{user_id}'")
-        self.databese.commit()
+        self.cursor.execute(f"DELETE FROM queue WHERE user_id = '{user_id}'")
+        self.database.commit()
 
     def delete_room(self, room_id):
-        self.cursor.executor(f"DELETE FROM rooms WHERE room_id = '{room_id}'")
+        self.cursor.execute(f"DELETE FROM rooms WHERE room_id = '{room_id}'")
         self.database.commit()
 
     def close(self):
